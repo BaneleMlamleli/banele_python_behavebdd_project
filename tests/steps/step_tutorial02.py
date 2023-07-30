9c01d50877cb34d1ac1995b18f6637f81615b994
KS_BaneleMlamleli <banele@kineticskunk.com>
2023-07-31 01:14:33 +0200

    Completed the Tutorial section. Tests all passed and I understand the implementation

--- tests/steps/step_tutorial02.py
+++ tests/steps/step_tutorial02.py
@@ -33,6 +33,17 @@ class NinjaFight(object):
 from behave   import given, when, then
 from hamcrest import assert_that, equal_to, is_not
 
+@given('the ninja encounters another opponent')
+def step_the_ninja_encounters_another_opponent(context):
+    """
+    BACKGROUND steps are called at begin of each scenario before other steps.
+    """
+    # -- SETUP/TEARDOWN:
+    if hasattr(context, "ninja_fight"):
+        # -- VERIFY: Double-call does not occur.
+        assert_that(context.ninja_fight, is_not(equal_to(None)))
+    context.ninja_fight = None
+    
 @given('the ninja has a {achievement_level}')
 def step_the_ninja_has_a(context, achievement_level):
     context.ninja_fight = NinjaFight(achievement_level)
